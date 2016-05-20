3fbfe5a55daccc16376ea8edfb40812807f2294a
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2016-05-20 12:35:08 +0200

    Init

---
+++ src/main/java/entities/MyEntity.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class MyEntity implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+
+	public MyEntity() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/launch/Launcher.java
@@ -0,0 +1,37 @@
+package launch;
+
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import entities.MyEntity;
+
+public class Launcher {
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = Logger.getLogger(Launcher.class.getCanonicalName());
+
+	public static void main(String[] args) {
+		LOGGER.info("Starting.");
+		new Launcher().launch();
+	}
+
+	@SuppressWarnings("boxing")
+	public void launch() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MyUnit");
+		EntityManager em1 = factory.createEntityManager();
+		EntityTransaction tx = em1.getTransaction();
+		tx.begin();
+		MyEntity myEntity = new MyEntity();
+		em1.persist(myEntity);
+		LOGGER.info("New entity id: " + myEntity.getId() + ".");
+		tx.commit();
+		tx.begin();
+		myEntity.setName("MyName");
+		tx.commit();
+		em1.close();
+		factory.close();
+	}
+}

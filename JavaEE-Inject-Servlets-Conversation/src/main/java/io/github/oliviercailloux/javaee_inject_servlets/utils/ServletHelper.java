953405301ac22c8c653ca33c92efc7f4297ab317
Olivier Cailloux <olivier.cailloux@gmail.com>
2017-04-09 18:07:55 +0200

    Helper RequestScoped

--- JavaEE-Inject-Servlets-Conversation/src/main/java/io/github/oliviercailloux/javaee_inject_servlets/utils/ServletHelper.java
+++ JavaEE-Inject-Servlets-Conversation/src/main/java/io/github/oliviercailloux/javaee_inject_servlets/utils/ServletHelper.java
@@ -1,14 +1,20 @@
 package io.github.oliviercailloux.javaee_inject_servlets.utils;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.Locale;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
 
 import io.github.oliviercailloux.javaee_inject_servlets.servlets.ViewConversationServlet;
 
-@ApplicationScoped
+@RequestScoped
 public class ServletHelper {
 	@Inject
 	private ServletContext context;
